45e46d2935169d3db1193b3c380760afb26ab9f5
Jacob Danovitch <jacobdanovitch@cmail.carleton.ca>
2018-03-15 20:10:57 -0400

    Bug fixes

--- webhook.py
+++ webhook.py
@@ -10,30 +10,28 @@ app = Flask(__name__)
 assist = Assistant(app, '/')
 url = "https://carleton.campusdish.com/Commerce/Catalog/Menus.aspx?LocationId=5087"
 
-menu = {}
-data = BeautifulSoup(requests.get(url).text, "html.parser")
-
-stations = data.find_all("div", {"class": "menu-details-station"})
-
-for station in stations:
-    name = station.find("h2", {"class": "collapsible-header"}).get_text()
-    if name in ["Grill", "Global", "Farmer’s Market"]:
-        menu[name] = [i.get_text() for i in station.find_all("a")]
-
-speech = ''
-
-for station in menu:
-    speech += 'At the ' + station + ', there is: '
-
-    for food in menu[station]:
-        speech += food + ', '
-
-    speech += '\n'
-print(speech)
-
 
 @assist.action("ask-for-menu")
 def ask_for_menu():
+    menu = {}
+    data = BeautifulSoup(requests.get(url).text, "html.parser")
+
+    stations = data.find_all("div", {"class": "menu-details-station"})
+
+    for station in stations:
+        name = station.find("h2", {"class": "collapsible-header"}).get_text()
+        if name in ["Grill", "Global", "Farmer’s Market"]:
+            menu[name] = [i.get_text() for i in station.find_all("a")]
+
+    speech = ''
+
+    for station in menu:
+        speech += 'At the ' + station + ', there is: '
+
+        for food in menu[station]:
+            speech += food + ', '
+
+        speech += '\n'
     return tell(speech)
 
 
